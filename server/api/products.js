a8a7a6a28e59a21959eb3745e42f28bf93621382
an-marryKyslenko <olih.a@icloud.com>
2023-06-11 14:52:18 +0200

    delete path

--- server/api/products.js
+++ server/api/products.js
@@ -3,11 +3,5 @@ const { getAllProducts } = require('../controllers/products')
 
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({ name: 'John Doe' })
-})
-
 router.route('/').get(getAllProducts)
 module.exports = router
